208ef027176984f690966156e0d76ebf3805ff73
KeyTer-01 <peterbabalola44@gmail.com>
2024-09-02 17:56:37 +0100

    edit lyrics

--- src/components/songs/PublishSalvation.js
+++ src/components/songs/PublishSalvation.js
@@ -4,18 +4,53 @@ import React from "react";
 export const PublishSalvation = () => {
   return (
     <Box>
-      <Text as={"b"}></Text>
-      <Text></Text>
-      <Text as={"b"}></Text>
-      <Text></Text>
-      <Text as={"b"}></Text>
-      <Text></Text>
-      <Text as={"b"}></Text>
-      <Text></Text>
-      <Text as={"b"}></Text>
-      <Text></Text>
-      <Text as={"b"}></Text>
-      <Text></Text>
+      <Text as={"b"}>Chorus</Text>
+      <Text>
+        How we heard of the tiding of your year
+        <br />
+        Where you published your peace
+        <br />
+        Now we long for the tiding of your day
+        <br />
+        Come and publish salvation
+        <br />
+        We will hear, we will hear
+        <br />
+        Until we can do
+        <br />
+        We will do, and we will do
+        <br />
+        Until we become
+      </Text>
+      <br />
+      <Text as={"b"}>Verse</Text>
+      <Text>
+        We want to know the Only True God
+        <br />
+        By the understanding that you brought
+        <br />
+        Son of God
+        <br />
+        The word that lives and abides forever
+        <br />
+        Sharper than any two-edged sword
+        <br />
+        Shine forth
+        <br />
+        <br />
+        We want to know the Only True God
+        <br />
+        And the Son who sits upon the right hand of the majesty on high
+      </Text>
+      <br />
+      <Text as={"b"}>Reprise</Text>
+      <Text>
+        Speak, speak, speak to us
+        <br />
+        Manifest Jesus
+        <br />
+        Immortality, dwelling in light
+      </Text>
     </Box>
   );
 };
